44bd65d14bdfb5c7a8a4f348b9fac69676c2391c
sljeff <shanliangjeff@qq.com>
2016-08-27 19:10:52 +0800

    coroutine

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+#!usr/bin/python3
 # coding: utf-8
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlencode
 import json
+import gevent.pool
+from gevent import monkey
+import os
+
+monkey.patch_all()
+
 
 def getBzhanInfoByKeywords(keyword='曾艳芬'):
     """
@@ -12,11 +19,16 @@ def getBzhanInfoByKeywords(keyword='曾艳芬'):
     """
     k = urlencode({'keyword': keyword})
     baseURL = 'http://search.bilibili.com/ajax_api/video?%s&order=totalrank&page=' % k
+
+    if not os.path.exists('img'):
+        os.mkdir('img')
+
     # 预处理
     preNum = 1
     preR = requests.get(baseURL + str(preNum))
     prerResult = preR.json()
     numPages = int(prerResult['numPages'])
+    pool = gevent.pool.Pool(50)
 
     def downloadImg(imgURL):
         """
@@ -26,14 +38,14 @@ def getBzhanInfoByKeywords(keyword='曾艳芬'):
         r = requests.get(imgURL)
         index = imgURL.index('archive') + 8
         saveName = str(imgURL[index:])
-        with open(saveName, 'wb') as f:
+        with open(os.path.join('img', str(saveName)), 'wb') as f:
             f.write(r.content)
 
     def getInfoFromLi(li):
         soupTemp = BeautifulSoup(str(li), "html.parser")
         avURL = str(soupTemp.a['href'])
         _avIndex = avURL.index('av')
-        avCode = avURL[_avIndex+2:-1]
+        avCode = avURL[_avIndex + 2:-1]
         shareURL = 'http://static.hdslb.com/miniloader.swf?aid=%s&page=1' % avCode
         title = soupTemp.img['title']
         imgURL = soupTemp.img['src']
@@ -50,30 +62,32 @@ def getBzhanInfoByKeywords(keyword='曾艳芬'):
         return info
 
     result = []
+    urls = []
 
     for i in range(1, numPages + 1):
         url = baseURL + str(i)
-        try:
-            r = requests.get(url)
-        except:
-            try:
-                r = requests.get(url)
-            except:
-                return json.dumps(result)
+        urls.append(url)
+    print('parsing......')
+    rs = pool.map(requests.get, urls)
+
+    for r in rs:
         html = r.json()['html']
         lis = getInfoFromHTML(html)
         for li in lis:
             temp = getInfoFromLi(li)
             result.append(temp)
             try:
-                # downloadImg(temp['imgURL'])
+                # pool.spawn(downloadImg(temp['imgURL']))
+                print(temp['imgURL'])
                 pass
             except:
-                print('error with %s'%temp['imgURL'])
-            finally:
-                print(temp['imgURL'])
+                print('error with %s' % temp['imgURL'])
+
+    print('ok!!!!!')
+    pool.join()
 
     return json.dumps(result)
+
 
 if __name__ == '__main__':
     r = getBzhanInfoByKeywords()
